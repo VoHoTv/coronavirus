f6b4371bb77ba52c85b8714b7b893dc94524cc80
VoHoTv <vohotv@gmail.com>
2020-03-24 23:15:15 +0100

    Switched from CURL to HTTP API

--- includes/class-coronavirus-shared-functionality.php
+++ includes/class-coronavirus-shared-functionality.php
@@ -32,18 +32,14 @@ class Coronavirus_Shared_Functionality {
 	 * Get the data of the API.
 	 */
     public function get_corona_data(string $country) {
-		$ch = curl_init();
-
 		$country_slug = str_replace(' ', '%20', $country);
-		// If country attribute is omitted display global data.
+		
+		// // If country attribute is omitted display global data.
 		$url = empty($country) ? "https://coronavirus-19-api.herokuapp.com/all" : "https://coronavirus-19-api.herokuapp.com/countries/$country_slug";
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-		$response = json_decode(curl_exec($ch));
+		$response = wp_remote_get( $url );
+		$body = json_decode(wp_remote_retrieve_body( $response ));
 
-		curl_close($ch);
-
-		return $response;
+		return $body;
 	}
 }
