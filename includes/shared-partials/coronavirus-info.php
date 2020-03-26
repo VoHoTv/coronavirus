256f391444588ae78b40e45b63f656e416b2d0bf
VoHoTv <vohotv@gmail.com>
2020-03-26 17:12:10 +0100

    Bugfix: Only show data when there is a response.

--- includes/shared-partials/coronavirus-info.php
+++ includes/shared-partials/coronavirus-info.php
@@ -3,24 +3,26 @@
     <?php echo empty($country_data->country) ? __('Globally', 'coronavirus') : esc_html__($country_data->country, 'coronavirus'); ?>
   </p>
   <div>
-  <?php foreach ($corona_data_options as $key => $option) : $string = preg_replace('/[A-Z]/', ' $0', $key); ?>
-    <!-- Get substring of the key to see if the first word is today. If it is we switch the first and last second word so we can omit hard coding key : value pairs-->
-    <!-- If country attribute is present -->      
-    <?php if ($option === 'on' && !empty($country_data->country)) : ?>
-        <?php if (substr($key, 0, 5) !== 'today') : ?>
-            <label class="panel-block" style="border-color: <?php echo esc_attr(get_option('border_color')); ?>; color: <?php echo esc_attr(get_option('general_text_color')); ?>!important;"><?php echo ucfirst(strtolower($string)) . ": " . esc_html__($country_data->$key); ?></label>
-          <?php elseif (substr($key, 0, 5) === 'today') : $pieces = preg_split('/(?=[A-Z])/',$key); ?>
-            <label class="panel-block" style="border-color: <?php echo esc_attr(get_option('border_color')); ?>; color: <?php echo esc_attr(get_option('general_text_color')); ?>!important;"><?php echo ucfirst($pieces[1]) . ' ' . strtolower($pieces[0]) . ": " . $country_data->$key; ?></label>
-        <?php endif ?>
-    <!-- If the country attribute is omitted -->      
-    <?php elseif (empty($country_data->country)) : ?>
-        <?php foreach ($country_data as $key => $value) : ?>
-            <?php if ($key !== 'updated') : ?>
-              <label class="panel-block" style="border-color: <?php echo esc_attr(get_option('border_color')); ?>; color: <?php echo esc_attr(get_option('general_text_color')); ?>!important;"><?php echo ucfirst($key) . ": $value"; ?></label>
-            <?php endif; ?>    
-        <?php endforeach; ?>
-        <?php break;?>
-    <?php endif; ?>
-  <?php endforeach; ?>
+  <?php if (!empty($corona_data_options) && !empty($country_data)): ?>
+    <?php foreach ($corona_data_options as $key => $option) : $string = preg_replace('/[A-Z]/', ' $0', $key); ?>
+      <!-- Get substring of the key to see if the first word is today. If it is we switch the first and last second word so we can omit hard coding key : value pairs-->
+      <!-- If country attribute is present -->      
+      <?php if ($option === 'on' && !empty($country_data->country)) : ?>
+          <?php if (substr($key, 0, 5) !== 'today') : ?>
+              <label class="panel-block" style="border-color: <?php echo esc_attr(get_option('border_color')); ?>; color: <?php echo esc_attr(get_option('general_text_color')); ?>!important;"><?php echo ucfirst(strtolower($string)) . ": " . esc_html__($country_data->$key); ?></label>
+            <?php elseif (substr($key, 0, 5) === 'today') : $pieces = preg_split('/(?=[A-Z])/',$key); ?>
+              <label class="panel-block" style="border-color: <?php echo esc_attr(get_option('border_color')); ?>; color: <?php echo esc_attr(get_option('general_text_color')); ?>!important;"><?php echo ucfirst($pieces[1]) . ' ' . strtolower($pieces[0]) . ": " . $country_data->$key; ?></label>
+          <?php endif ?>
+      <!-- If the country attribute is omitted -->      
+      <?php elseif (empty($country_data->country)) : ?>
+          <?php foreach ($country_data as $key => $value) : ?>
+              <?php if ($key !== 'updated') : ?>
+                <label class="panel-block" style="border-color: <?php echo esc_attr(get_option('border_color')); ?>; color: <?php echo esc_attr(get_option('general_text_color')); ?>!important;"><?php echo ucfirst($key) . ": $value"; ?></label>
+              <?php endif; ?>    
+          <?php endforeach; ?>
+          <?php break;?>
+      <?php endif; ?>
+    <?php endforeach; ?>
+  <?php endif; ?>
   </div>
 </article>
