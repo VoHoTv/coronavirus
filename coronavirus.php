4249a32a513ea901ae838fffe8b0e8eaeafd33f0
VoHoTv <vohotv@gmail.com>
2020-03-26 20:04:22 +0100

    Updated plugin info in coronavirus.php

--- coronavirus.php
+++ coronavirus.php
@@ -15,8 +15,8 @@
  * @wordpress-plugin
  * Plugin Name:       Coronavirus
  * Plugin URI:        https://github.com/VoHoTv/coronavirus
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           1.0.0
+ * Description:       Display data about the coronavirus.
+ * Version:           1.1.0
  * Author:            Mike Zuidhoek
  * Author URI:        https://mikezuidhoek.com/
  * License:           GPL-2.0+
